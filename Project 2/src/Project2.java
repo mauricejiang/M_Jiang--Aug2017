f78789842b189878a51ca7bb357aa831492cff18
mauricejiang <19jiangm06@millshighsch.org>
2017-08-22 09:28:08 -0700

    HI people

---
+++ Project 2/src/Project2.java
@@ -0,0 +1,13 @@
+// My name is Maurice
+// It is my second Java project
+// Today is Aug. 16 2017
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		System.out.println(" this is project 2");
+		System.out.print("Hello");
+		System.out.println(" this is project 2");
+	}
+
+}
