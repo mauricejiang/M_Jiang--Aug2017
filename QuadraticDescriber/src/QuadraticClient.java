a6b8f6b829371fd02d9dcf707cb3b343d25f1d07
mauricejiang <31105391+mauricejiang@users.noreply.github.com>
2017-10-31 18:34:51 -0700

    Update QuadraticClient.java

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,7 +1,27 @@
 import java.util.*;
 public class QuadraticClient {
-	//Scanner coef = new Scanner (System.in);
-	System.out.println("Welcome to quadratic equation ax^2 + bx +c");
+
 	
+	finish=false	
+	if(finish==false){
+	Scanner input = new Scanner (System.in);
+	System.out.println("Welcome to quadratic equation ax^2 + bx +c");
+	System.out.println("Enter value a:");
+	double a = input.nextInt()
+	System.out.println("Enter value b:");
+	double b = input.nextInt()
+	System.out.println("Enter value c:");
+	double c = input.nextInt();
+	
+	System.out.println(Quadratic.quadrDescriber(a, b, c));
+
+			
+	System.out.print("\n\Are you done?(y/n): ");
+	String decide = input.next();
+	if ((decide.equals("n"))) {
+		System.out.println("\nThank you for using this program!");
+		finish = true;
+	}
+}
 	
 }
