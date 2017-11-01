f6c6a97bec2d9ad7b281fbf0e59f9c092e762669
mauricejiang <31105391+mauricejiang@users.noreply.github.com>
2017-10-31 17:35:55 -0700

    Update QuadraticClient.java

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class QuadraticClient {
-	Scanner coef = new Scanner (System.in);
+	//Scanner coef = new Scanner (System.in);
 	System.out.println("Welcome to quadratic equation ax^2 + bx +c");
+	
 	
 }
