3e3d232a8fd16cc78df5e6c5357b1b8b98cf6a62
mauricejiang <19jiangm06@millshighsch.org>
2017-11-02 08:54:48 -0700

    hi

---
+++ SplitString/src/LotsOfCopies.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+	public class LotsOfCopies {
+		public static void main(String[] args) {
+			int num = 7;
+			String strMain = "APCS";
+			int[] arrMain = {1, 2, 3, 4, 5};
+			changeMe(num, strMain, arrMain);
+			System.out.println(num);
+			System.out.println(strMain);
+			System.out.println(Arrays.toString(arrMain));
+		}
+		public static void changeMe (int x, String str, int arr[]) {
+			x=8;
+			str="10";
+			for(int i = 0; i<arr.length; i++) {
+				arr[i]++;
+			}
+		}
+	}
